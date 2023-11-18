26ee0e005e6935faed751a147333f02bd51e4c8a
Aman <amangarg0303@gmail.com>
2023-11-19 01:25:51 +0530

    dashboard posts (see your own posts)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,29 +1,41 @@
 import Post from "@/components/Post";
-import { postsData } from "@/data";
 import Link from "next/link";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+
+const getPosts = async (email: string) => {
+  try {
+    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/authors/${email}`);
+    if (res.ok) {
+      const { posts } = await res.json();
+      return posts;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const Dashboard = async () => {
   const session = await getServerSession(authOptions);
   if (!session) {
     redirect("/signin");
   }
+  const posts = await getPosts(session?.user?.email as string);
 
   return (
     <div>
       <h1 className="text-2xl font-bold">My Posts</h1>
-      {postsData && postsData.length > 0 ? (
-        postsData?.map((post) => (
+      {posts && posts.length > 0 ? (
+        posts?.map((post: any) => (
           <Post
             key={post.id}
             id={post.id}
-            author={post.author}
-            authorEmail={"test@gmail.com"}
-            date={post.datepublished}
-            thumbnail={post.thumbnail}
-            category={post.category}
+            author={session?.user?.name!}
+            authorEmail={post?.authorEmail}
+            date={post.createdAt}
+            thumbnail={post.imageUrl}
+            category={post.catName}
             title={post.title}
             desc={post.content}
             links={post.links || []}
