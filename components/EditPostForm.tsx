59ca724378cd996a7d15850e7c26955f653e81e9
Aman <amangarg0303@gmail.com>
2023-11-23 21:12:47 +0530

    edit an image in post

--- components/EditPostForm.tsx
+++ components/EditPostForm.tsx
@@ -2,6 +2,8 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { CldUploadButton, CldUploadWidgetResults } from "next-cloudinary";
+import Image from "next/image";
 
 const EditPostForm = ({ post }: any) => {
   const [links, setLinks] = useState<string[]>(post.links || []);
@@ -42,6 +44,36 @@ const EditPostForm = ({ post }: any) => {
 
   const handleChange = (e: any) => {
     setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleUploadImage = (result: CldUploadWidgetResults) => {
+    const info = result.info as object;
+
+    if ("secure_url" in info && "public_id" in info) {
+      const url = info.secure_url as string;
+      const public_id = info.public_id as string;
+      setData({ ...data, imageUrl: url, publicId: public_id });
+    }
+  };
+
+  const handleRemoveImg = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(`/api/removeImage`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ publicId: data.publicId }),
+      });
+
+      if (res.ok) {
+        setData({ ...data, imageUrl: "", publicId: "" });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -150,6 +182,49 @@ const EditPostForm = ({ post }: any) => {
             Add
           </button>
         </div>
+
+        <CldUploadButton
+          uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET}
+          className={`h-48 border-2 mt-4 border-dotted grid place-items-center bg-slate-100 rounded-md relative ${
+            data.imageUrl && "pointer-events-none"
+          } `}
+          onUpload={handleUploadImage}
+        >
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+              />
+            </svg>
+          </div>
+
+          {data.imageUrl && (
+            <Image
+              src={data.imageUrl}
+              fill
+              className="absolute object-cover inset-0"
+              alt={data.title}
+            />
+          )}
+        </CldUploadButton>
+
+        {data.publicId && (
+          <button
+            onClick={handleRemoveImg}
+            className="py-2 px-4 rounded-md font-semibold w-fit bg-red-600 text-white mb-4"
+          >
+            Remove Image
+          </button>
+        )}
 
         <select
           onChange={handleChange}
